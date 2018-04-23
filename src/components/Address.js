f8e79b37cdee92b10581160cd972d5b0fc05c977
vsparrow <khan@vsparrow.com>
2018-04-23 16:21:56 -0400

    Done.

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,1 +1,18 @@
 // Component Code Goes Here
+// the default state of this component is to be in the off state
+import React from 'react';
+class Address extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { fullAddress: `${props.street},${props.city}`}
+  }
+  render(){
+    return(
+      <div className="address">
+        {/* { this.state.fullAddress}  below is better than this */}
+        {this.props.street}, {this.props.city}
+      </div>
+    );
+  }
+}//class
+export default Address;
